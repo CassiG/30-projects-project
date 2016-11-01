5f18f28b71cf02dfc6a2ddd83cfd22b7b1e10988
Cassandra Gallagher <ccgall@gmail.com>
2016-11-01 14:41:28 -0700

    create header and footer files

---
+++ index.php
@@ -0,0 +1,19 @@
+  <?php
+    include("includes/header.html");
+  ?>
+
+  <div class="main-content">
+
+    <div class="col first">
+      <p>Vexillologist pork belly typewriter la croix iPhone, thundercats photo booth schlitz gluten-free vinyl lumbersexual actually. Readymade tofu cray 3 wolf moon hella. Knausgaard DIY tbh, microdosing poutine street art venmo. Trust fund la croix distillery drinking vinegar hella. Church-key vinyl godard pour-over thundercats, williamsburg lumbersexual 90's squid hot chicken stumptown. Fixie marfa drinking vinegar VHS kitsch flannel sriracha. Poutine jean shorts hashtag flexitarian, twee chicharrones mixtape keytar man braid direct trade tofu umami lumbersexual green juice.</p>
+    </div>
+
+    <div class="col second">
+      <p>Activated charcoal occupy master cleanse succulents. Occupy listicle gluten-free tofu. Bicycle rights food truck kogi actually, tote bag offal kombucha shoreditch hashtag semiotics pug. Yuccie offal butcher keffiyeh health goth. Unicorn mumblecore copper mug deep v distillery hexagon tbh affogato, next level raw denim PBR&B crucifix. Dreamcatcher pok pok fam tousled, helvetica four dollar toast semiotics retro. Wayfarers raw denim viral microdosing williamsburg.</p>
+    </div>
+
+  </div>
+
+  <?php
+    include("includes/footer.html");
+  ?>
