94789790046714c0177c24b3c4d087e325db3760
Cassi Gallagher <ccgall@gmail.com>
2017-09-16 19:54:52 -0700

    Create user model and controller, set up bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915035026) do
+ActiveRecord::Schema.define(version: 20170917024556) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.text "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_hash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
